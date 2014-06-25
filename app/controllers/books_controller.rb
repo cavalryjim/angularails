e79c050d1c34347bddae4cbb4acc44448e327f8e
Greg Schulte <rikkitikkitumbo@gmail.com>
2014-06-25 14:50:42 -0700

    first

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,84 @@
+class BooksController < ApplicationController
+  before_action :set_book, only: [:show, :edit, :update, :destroy]
+
+  # GET /books
+  def index
+    @books = Book.all
+
+    respond_to do |format|
+      format.html {
+        @books_json = @books.map{ |b| BookSerializer.new(b).serializable_hash }
+        @urls = {
+            books: books_path
+        }
+      }
+      format.json { render json: @books }
+    end
+  end
+
+
+  # GET /books/1
+  def show
+  end
+
+  # GET /books/new
+  def new
+    @book = Book.new
+  end
+
+  # GET /books/1/edit
+  def edit
+  end
+
+  # POST /books
+  def create
+    @book = Book.create(book_params)
+
+    respond_to do |format|
+      if @book.save
+        format.html { redirect_to @book, notice: 'Book was successfully created.' }
+        format.json { render json: @book }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @book.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
+  # PATCH/PUT /books/1
+  def update
+    respond_to do |format|
+      if @book.update(book_params)
+        format.html {redirect_to @book, notice: 'book successfully updated'}
+        format.json { render json: @book }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @book.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /books/1
+  def destroy
+    @book.destroy
+    respond_to do |format|
+      format.html {
+        redirect_to books_url, notice: 'Book was successfully destroyed.'
+      }
+      format.json { render json: { message: "Book was deleted"} }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_book
+      @book = Book.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def book_params
+      params.require(:book).permit(:title, :author)
+    end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Angularails::Application.config.secret_key_base = '99da32a01906fda160d38332648c4151b369f4505e7e0e4f86eae64ceb900e3962fd9d8cdf38a85445590e4a7b8cb2b3b7a80ca762a9706b37759c98820eddd1'
